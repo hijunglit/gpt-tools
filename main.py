324f4d770a7c72defa903f1d4a72d15752c46c2a
hijunglit <hijunglit@gmail.com>
2024-11-08 23:42:47 +0900

    recap oop

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 class Puppy:
+
     def __init__(self, name, breed):
         self.name = name
         self.age = 0.1
         self.breed = breed
 
     def __str__(self):
-        return f"Puppy named {self.name} and breed is {self.breed}"
+        return f"{self.breed} puppy named {self.name}"
 
-ruffus = Puppy(
-    name="wangja",
-    breed="jindo"
-)
-bibi = Puppy(
-    name="gongju",
-    breed="jindo"
-)
 
-print(
-    ruffus,
-    bibi
-)
+wangja = Puppy("Wangja", "Jindo")
+gongju = Puppy("Gongju", "jindo")
+
+print(wangja, gongju)

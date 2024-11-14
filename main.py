251524e869e1d08ace8d18540e3cda938829ca2e
hijunglit <hijunglit@gmail.com>
2024-11-14 23:59:26 +0900

    web scrapper ...

--- main.py
+++ main.py
@@ -1,50 +1,9 @@
-class Player:
+import requests
 
-    def __init__(self, name, team):
-        self.name = name
-        self.xp = 1500
-        self.team = team
+url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
-    def introduce(self):
-        print(f"hello my name is {self.name}. and I play for {self.team}")
+response = requests.get(url)
 
-class Team:
+response.status_code
 
-    def __init__(self, team_name):
-        self.team_name = team_name
-        self.players = []
-
-    def add_player(self, name):
-        new_player = Player(name, self.team_name)
-        self.players.append(new_player)
-    
-    def show_players(self):
-        for player in self.players:
-            player.introduce()
-    def remove_players(self, name):
-        for target_player in self.players:
-            if target_player.name == name:
-                self.players.remove(target_player)
-            else:
-                print("This value does not exist")
-    def total_xp(self):
-        xp = (0)
-        for player in self.players:
-            xp=xp + player.xp
-            print(f"{self.team_name} total xp is {xp}")
-
-team_red = Team("Team red")
-
-team_red.add_player("Haein")
-team_red.add_player("John doe")
-team_red.add_player("chris")
-team_red.add_player("harry")
-
-team_red.remove_players("chris")
-
-team_red.show_players()
-
-team_red.total_xp()
-
-# 챌린지: 팀 클래스에서 플레이어 삭제 메소드 만들기. => remove 메소드 활용
-# 챌린지: 팀 클래스에서 경험치의 총 합을 보여주는 메소드 만들기. 
+print(response.content)

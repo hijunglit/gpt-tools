60328533cd0a924fb4522ad6ad8c5444ee2b65cd
hijunglit <hijunglit@gmail.com>
2024-11-17 23:37:30 +0900

    git push

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ soup = BeautifulSoup(
     response.content,
     "html.parser",
 )
-jobs = soup.find("section", class_="jobs").find_all("li")
-print(jobs)
+jobs = soup.find("section",
+class_="jobs").find_all("li")
+
+for job in jobs:
+    title = job.find("span", class_="title")
+    region = job.find("span", class_="region")
+    company, position, _ =  job.find_all("span", class_ ="company")

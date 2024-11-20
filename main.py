e68bcb529ce4b21baa52a99752d1a34f69a3a4b2
hijunglit <hijunglit@gmail.com>
2024-11-20 23:52:20 +0900

    job scrapper ...

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ soup = BeautifulSoup(
 jobs = soup.find("section",
 class_="jobs").find_all("li")
 
+all_jobs=[]
+
 for job in jobs:
     title = job.find("span", class_="title").text
     company, position, region =  job.find_all("span", class_ ="company")
@@ -19,3 +21,10 @@ for job in jobs:
     position = position.text
     region = region.text
     print(title, company, position, region, "--------\n")
+    url = job.find("href")
+    job_data = {
+        "title": title,
+        "company": company.text,
+        "position": position.text
+    }
+    all_jobs.append(job_data)

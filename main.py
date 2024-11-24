2c0a5521180d93962a641a4ba470cc8227e6d170
hijunglit <hijunglit@gmail.com>
2024-11-25 00:06:52 +0900

    find all list

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
+
+jobs = soup.find("section", class_ = "jobs").find_all("li") ## class라는 단어는 python 언어. class 를 만들 때 사용하는 것으로 예약되어 있다.
+
+print(jobs)

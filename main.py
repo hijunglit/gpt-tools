c1be5f06ad114cfa1960cd321ac22a7031778ef1
hijunglit <hijunglit@gmail.com>
2024-11-27 01:00:33 +0900

    pagenation ...

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
+all_jobs = []
 
-response = requests.get(url)
+def scrape_page(url):
+    response = requests.get(url)
 
-soup = BeautifulSoup(response.content, "html.parser")
+    soup = BeautifulSoup(response.content, "html.parser")
 
-jobs = soup.find("section", class_ = "jobs").find_all("li")[0:-1] ## class라는 단어는 python 언어. class 를 만들 때 사용하는 것으로 예약되어 있다.
+    jobs = soup.find("section", class_ = "jobs").find_all("li")[0:-1] ## class라는 단어는 python 언어. class 를 만들 때 사용하는 것으로 예약되어 있다.
+    for job in jobs:
+        title = job.find("span", class_="title").text ## 텍스트를 추출하고 싶으면 text() method
+        company, position, region = job.find_all("span", class_="company")
+        url = job.find("div", class_="tooltip").next_sibling["href"]
+        job_data = {
+            "title": title,
+            "company": company.text,
+            "position": position.text,
+            "region": region.text,
+            "url": f"https://weworkremotely.com{url}",
+        }
+        all_jobs.append(job_data)
+        company = company.text
+        position = position.text
+        region = region.text
+        print(title, company, region, "------\n")
 
-for job in jobs:
-    title = job.find("span", class_="title").text ## 텍스트를 추출하고 싶으면 text() method
-    company, position, region = job.find_all("span", class_="company")
-    company = company.text
-    position = position.text
-    region = region.text
-    print(title, company, region, "------\n")
